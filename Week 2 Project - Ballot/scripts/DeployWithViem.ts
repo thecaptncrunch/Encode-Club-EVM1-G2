f275d5735f18acfac0f59f82f9d687d3f1f39a57
Evelin <evelinrpaumgertner@hotmail.com>
2025-03-16 14:48:57 -0300

    Creating a wallet client

--- Week 2 Project - Ballot/scripts/DeployWithViem.ts
+++ Week 2 Project - Ballot/scripts/DeployWithViem.ts
@@ -1,9 +1,13 @@
-import { createPublicClient, http } from "viem";
+import { createPublicClient, http, createWalletClient, formatEther } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
 import { sepolia } from "viem/chains";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+
+const deployerPrivateKey = process.env.PRIVATE_KEY || "";
+
 
 async function main() {
   const proposals = process.argv.slice(2);
@@ -15,7 +19,24 @@ async function main() {
   });
   const blockNumber = await publicClient.getBlockNumber();
   console.log("Last block number:", blockNumber);
+
+  const account = privateKeyToAccount(`0x${deployerPrivateKey}`);
+  const deployer = createWalletClient({
+    account,
+    chain: sepolia,
+    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+  });
+  console.log("Deployer address:", deployer.account.address);
+  const balance = await publicClient.getBalance({
+    address: deployer.account.address,
+  });
+  console.log(
+    "Deployer balance:",
+    formatEther(balance),
+    deployer.chain.nativeCurrency.symbol
+  );
 }
+
 
 main().catch((error) => {
   console.error(error);
